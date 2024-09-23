ce54c1a2c4cee6c77df7aba4577e1e5e2c578e5b
raxiv2 <gg2946@rit.edu>
2024-09-23 18:55:25 -0400

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,36 @@
+##---------------------------------------------------------------------------
+## Mia's score
+##---------------------------------------------------------------------------
+
+## Assign the string "Mia,100" to a variable name_grade
+
+## Convert the string to a list of strings: ['Mia', '100']
+   ## Hint: use the split() method for strings
+
+## Extract Mia's score from the list and assign it to variable named score,
+## using list indexing
+
+## What is the type of the score at this point?
+
+## Convert the score to a float, and reassign it to variable named score.
+
+##---------------------------------------------------------------------------
+## Dave's score
+##---------------------------------------------------------------------------
+
+## Assign the string "Dave,90" to a variable name_grade
+
+## Convert the string to a list of strings, which will be ['Dave', '90']
+   ## Hint: use the split() method for strings
+
+## Extract Dave's score from the list and assign it to variable score,
+## using list indexing
+
+## What is the type of the score at this point?
+
+## Convert the score to a float, and reassign it to variable named score.
+
+##---------------------------------------------------------------------------
+## Converting the code above into a function
+##---------------------------------------------------------------------------
+
